25758aebcd7fe8bd3c30d2ce72440f6a972dedf4
Dimka863 <dlysachenko98@gmail.com>
2024-05-12 23:24:59 +0200

    validation

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -5,6 +5,8 @@ import { MovieSearch } from "../components/MovieSearch";
 import { useSearchParams } from "react-router-dom";
 import { Loader } from "../components/Loader";
 import { Error } from "../components/Error";
+import { Bounce, ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Movies = () => {
   const [movies, setMovies] = useState([]);
@@ -33,12 +35,27 @@ const Movies = () => {
     findMovies();
   }, [query, searchParams]);
   const handleSubmit = (values) => {
+    if (values.query.trim() === "") {
+      toast.error("Wrong input value!", {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+        transition: Bounce,
+      });
+      return;
+    }
     setSearchParams({ query: values.query });
     setQuery(values.query);
   };
   return (
     <div className="px-10 py-20">
       <MovieSearch handleSubmit={handleSubmit} />
+      <ToastContainer />
       {error && <Error />}
       {loading && <Loader />}
       {movies.length > 0 && !loading && !error && (
